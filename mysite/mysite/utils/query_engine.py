65c56f37877ee95ede9e4fcb6c2a64bd532b5c1b
Quant Lounge <7818207@qq.com>
2021-08-22 14:14:05 +0100

    Fixed getFilterData

--- mysite/mysite/utils/query_engine.py
+++ mysite/mysite/utils/query_engine.py
@@ -6,11 +6,12 @@
 
 
 
-import MySQLdb
+import mysql.connector
 from mysite.utils.naming import names_map
 import json
 import pandas as pd
 import math
+import numpy as np
 
 millnames = ['','K','M','B','T']
 
@@ -27,6 +28,7 @@ class bcolors:
 
 def millify(n):
     n = float(n)
+    print(n)
     millidx = max(0,min(len(millnames)-1,
                         int(math.floor(0 if n == 0 else math.log10(abs(n))/3))))
 
@@ -37,7 +39,7 @@ def query_from_db(raw_json):
     
 
 def add_filter(filter_name):
-    conn = MySQLdb.connect('194.163.166.72','stk','qqwrv!123','stock')
+    conn = mysql.connector.connect('localhost','stk','qqwrv!123','stock')
     curs = conn.cursor()
     result = select(names_map['Shares (Diluted)'],conn)
 
@@ -45,15 +47,19 @@ def add_filter(filter_name):
 def get_filter_df_from_db(filter_abv):
     #database_conn()
     print(bcolors.OKGREEN + '[QUery_Engine(get_filter_df_from_db)]: ' + bcolors.ENDC + 'Requesting DB')
-    conn = MySQLdb.connect('194.163.166.72','stk','qqwrv!123','stock')
+    conn = mysql.connector.connect(host='localhost',user='stk',password='qqwrv!123',database='stock')
     df = pd.read_sql("""
         SELECT Ticker,{0} FROM financial WHERE Fiscal_Year > '2019-01-01';
         """.format(filter_abv), con=conn)
+    print(bcolors.OKGREEN + '[QUery_Engine(get_filter_df_from_db)]: ' + bcolors.ENDC + 'Selected from DB: ' +filter_abv)
+    conn.close()
+    df.dropna(inplace=True)
     return df
 
-def get_bins_SHARES_DILUTED(df):
-    df['log_normal'] = df['SHARES_DILUTED'].apply(lambda x: np.log(x+1))
-    df['cut'] = pd.cut(df['log_normal'],bins=100)
+def normalize_for_display(df,filter):
+    if filter == 'SHARES_BASIC':
+        df['normal'] = df[filter].apply(lambda x: np.log(x+1))
+        df['cut'] = pd.cut(df['normal'],bins=100)
     
     
 def get_bins(serie,correction_coef):
@@ -73,13 +79,8 @@ def get_real_histogram_data(serie):
     vc = out.value_counts(sort=False)
     return bins, vc
 
-def generate_histogram_json(bins, vc):
-    data = [{'x0':a,'x':a+1,'y':b} for a,b in zip(range(0,100),vc.tolist())]
-    js = json.dumps(data)
-    js = "{" + js + "}"
-    
 def remove_outliers(serie):
-    serie.dropna(inplace=True)
+    #serie.dropna(inplace=True)
     lessthan = serie < np.percentile(serie,97)
     largerthan = serie > np.percentile(serie,3)
     mask = largerthan * lessthan
@@ -93,7 +94,7 @@ def generate_histogram_json(bins, vc):
     return js
 
 
-def get_bin_mapping(real_bins):
+def get_bin_mapping(df,real_bins):
     bin_df = pd.DataFrame(real_bins)
     bin_df['real_value'] = bin_df[0].apply(lambda x: np.exp(x) - 1)
     bin_df['readable'] = bin_df['real_value'].apply(lambda x: millify(x))
@@ -103,5 +104,14 @@ def get_filter_data(filter):
     filter_name = names_map[filter]
     print(bcolors.OKGREEN + '[QUery_Engine(get_filter_data)]: ' + bcolors.ENDC + 'The abv. name of the filter requested: ' +filter_name)
     df = get_filter_df_from_db(filter_name)
-    return 0,0
+    map,data = get_display_data(df,filter_name)
+    return map,data
     
+def get_display_data(df,filter):
+    out_lier_remove = remove_outliers(df[filter])
+    normalize_for_display(df,filter)
+    bins, vc = get_display_histogram_data(out_lier_remove)
+    real_bins, _ = get_real_histogram_data(df['normal'])
+    map = json.dumps(get_bin_mapping(df,real_bins))
+    data = generate_histogram_json(bins, vc)
+    return map,data
