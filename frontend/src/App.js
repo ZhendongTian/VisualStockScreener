bcb564a60d334724db868c465236374b7861ae6c
yczh <yc_zhu@foxmail.com>
2021-08-23 00:47:09 +0100

    update select

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import { AudioOutlined,UserOutlined, LaptopOutlined, NotificationOutlined } from
 import { useState } from "react"
 import DataFinder from "./components/DataFinder";
 import Filters from './components/Filters';
-
 const { Search } = Input;
 const { SubMenu } = Menu;
 const { Header, Content, Sider } = Layout;
@@ -30,7 +29,21 @@ function App() {
   }
   return (
     <Layout>
-    
+    <Header style={{backgroundColor:'white'}} className="header">
+      <div  className="logo" >
+      <span style={{color:'black'}}>Visual stock logo</span>
+      <Search placeholder="Search company" onSearch={onSearch} style={{ width: 300,padding:20 }} />
+      </div>
+      <Menu inlineCollapsed={false} theme="light" mode="horizontal" >
+        {new Array(1).fill(null).map((_, index) => {
+          const key = index + 1;
+          return <Menu.Item key={key}>{`nav ${key}`}</Menu.Item>;
+        })}
+        <Menu.Item key={0}>1</Menu.Item>
+        <Menu.Item key={1}>2</Menu.Item>
+        <Menu.Item key={2}><UserOutlined /></Menu.Item>
+      </Menu>
+    </Header>
     <Layout>
       <Sider width={350} style={{backgroundColor:'white',marginLeft:'20px'}} className="site-layout-background">
         <Filters></Filters>
--- frontend/src/components/HistoSelector.js
+++ frontend/src/components/HistoSelector.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
-import {Card} from 'antd'
+import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
 import PropTypes from 'prop-types'
 import Histoslider from 'histoslider';
 import { filter } from 'd3-array';
+import './select.css'
+import { transform } from '@babel/core';
 const handleSelect = (value)=>{
      console.log(value)
 }
+const backClick = (e)=>{
+  // console.log("back clicked")
+}
+
+
 
 class HistoSelector extends Component {
     static propTypes = {
@@ -16,26 +24,28 @@ class HistoSelector extends Component {
         data:[{'x0': 0, 'x': 1, 'y': 2}, {'x0': 1, 'x': 2, 'y': 7}, {'x0': 2, 'x': 3, 'y': 5}, {'x0': 3, 'x': 4, 'y': 6}, {'x0': 4, 'x': 5, 'y': 5}, {'x0': 5, 'x': 6, 'y': 3}, {'x0': 6, 'x': 7, 'y': 8}, {'x0': 7, 'x': 8, 'y': 8}, {'x0': 8, 'x': 9, 'y': 5}, {'x0': 9, 'x': 10, 'y': 5}, {'x0': 10, 'x': 11, 'y': 6}, {'x0': 11, 'x': 12, 'y': 10}, {'x0': 12, 'x': 13, 'y': 10}, {'x0': 13, 'x': 14, 'y': 11}, {'x0': 14, 'x': 15, 'y': 7}, {'x0': 15, 'x': 16, 'y': 9}, {'x0': 16, 'x': 17, 'y': 11}, {'x0': 17, 'x': 18, 'y': 9}, {'x0': 18, 'x': 19, 'y': 10}, {'x0': 19, 'x': 20, 'y': 18}, {'x0': 20, 'x': 21, 'y': 14}, {'x0': 21, 'x': 22, 'y': 9}, {'x0': 22, 'x': 23, 'y': 16}, {'x0': 23, 'x': 24, 'y': 11}, {'x0': 24, 'x': 25, 'y': 19}, {'x0': 25, 'x': 26, 'y': 13}, {'x0': 26, 'x': 27, 'y': 13}, {'x0': 27, 'x': 28, 'y': 18}, {'x0': 28, 'x': 29, 'y': 19}, {'x0': 29, 'x': 30, 'y': 24}, {'x0': 30, 'x': 31, 'y': 28}, {'x0': 31, 'x': 32, 'y': 23}, {'x0': 32, 'x': 33, 'y': 21}, {'x0': 33, 'x': 34, 'y': 18}, {'x0': 34, 'x': 35, 'y': 34}, {'x0': 35, 'x': 36, 'y': 35}, {'x0': 36, 'x': 37, 'y': 25}, {'x0': 37, 'x': 38, 'y': 30}, {'x0': 38, 'x': 39, 'y': 29}, {'x0': 39, 'x': 40, 'y': 36}, {'x0': 40, 'x': 41, 'y': 27}, {'x0': 41, 'x': 42, 'y': 44}, {'x0': 42, 'x': 43, 'y': 36}, {'x0': 43, 'x': 44, 'y': 41}, {'x0': 44, 'x': 45, 'y': 47}, {'x0': 45, 'x': 46, 'y': 27}, {'x0': 46, 'x': 47, 'y': 35}, {'x0': 47, 'x': 48, 'y': 46}, {'x0': 48, 'x': 49, 'y': 30}, {'x0': 49, 'x': 50, 'y': 30}, {'x0': 50, 'x': 51, 'y': 25}, {'x0': 51, 'x': 52, 'y': 26}, {'x0': 52, 'x': 53, 'y': 37}, {'x0': 53, 'x': 54, 'y': 19}, {'x0': 54, 'x': 55, 'y': 33}, {'x0': 55, 'x': 56, 'y': 27}, {'x0': 56, 'x': 57, 'y': 33}, {'x0': 57, 'x': 58, 'y': 45}, {'x0': 58, 'x': 59, 'y': 33}, {'x0': 59, 'x': 60, 'y': 30}, {'x0': 60, 'x': 61, 'y': 32}, {'x0': 61, 'x': 62, 'y': 30}, {'x0': 62, 'x': 63, 'y': 29}, {'x0': 63, 'x': 64, 'y': 27}, {'x0': 64, 'x': 65, 'y': 23}, {'x0': 65, 'x': 66, 'y': 15}, {'x0': 66, 'x': 67, 'y': 20}, {'x0': 67, 'x': 68, 'y': 15}, {'x0': 68, 'x': 69, 'y': 18}, {'x0': 69, 'x': 70, 'y': 26}, {'x0': 70, 'x': 71, 'y': 22}, {'x0': 71, 'x': 72, 'y': 23}, {'x0': 72, 'x': 73, 'y': 18}, {'x0': 73, 'x': 74, 'y': 23}, {'x0': 74, 'x': 75, 'y': 14}, {'x0': 75, 'x': 76, 'y': 18}, {'x0': 76, 'x': 77, 'y': 9}, {'x0': 77, 'x': 78, 'y': 14}, {'x0': 78, 'x': 79, 'y': 18}, {'x0': 79, 'x': 80, 'y': 19}, {'x0': 80, 'x': 81, 'y': 13}, {'x0': 81, 'x': 82, 'y': 16}, {'x0': 82, 'x': 83, 'y': 9}, {'x0': 83, 'x': 84, 'y': 11}, {'x0': 84, 'x': 85, 'y': 13}, {'x0': 85, 'x': 86, 'y': 9}, {'x0': 86, 'x': 87, 'y': 13}, {'x0': 87, 'x': 88, 'y': 14}, {'x0': 88, 'x': 89, 'y': 9}, {'x0': 89, 'x': 90, 'y': 7}, {'x0': 90, 'x': 91, 'y': 6}, {'x0': 91, 'x': 92, 'y': 12}, {'x0': 92, 'x': 93, 'y': 9}, {'x0': 93, 'x': 94, 'y': 2}, {'x0': 94, 'x': 95, 'y': 6}, {'x0': 95, 'x': 96, 'y': 5}, {'x0': 96, 'x': 97, 'y': 3}, {'x0': 97, 'x': 98, 'y': 2}, {'x0': 98, 'x': 99, 'y': 7}, {'x0': 99, 'x': 100, 'y': 4}],
         perc2value:[],
         selection:[0,100],
-        isSelected:false
+        isSelected:false,
+        isModalVisible:false
     }
     componentWillMount(){
-      console.log(this.props.name)
       fetch('http://194.163.166.72:8000/getFilterData?name='+this.props.name)
         .then(res=>res.json())
         .then(data=>{
-          console.log(data)
-          this.setState({data:data.data,perc2value:data.map})
-        })
+          let map = JSON.parse(data['map']) 
+          let data1 =JSON.parse(data['data']) 
+            this.setState({
+              data:data1,
+              perc2value:map})
+          })
+          
     }
     mouseclick = (e)=>{
-      this.state.isSelected=true;
+      console.log("circle clicked")
       const post ={
         x0:this.state.selection[0],
         x1:this.state.selection[1],
         name:this.state.name
       }
-      
-      console.log(this.state.selection);
       fetch('http://194.163.166.72:8000/onCriteriaChange',{
         method:'POST',
         headers:{
@@ -43,16 +53,79 @@ class HistoSelector extends Component {
         },
         body:JSON.stringify(post)
       }).then(res=>res.json())
-      .then(data=>console.log(data))
+      .then(data=>{})
     }
     componentDidMount(){
-      window.addEventListener('click',this.mouseclick)
+      const dom = ReactDOM.findDOMNode(this);
+      console.log(dom)
+      let circles = dom.getElementsByTagName('circle')
+      let text = dom.getElementsByTagName('text')
+      text[0].style.setProperty('transform',"translateY(13px)")
+      for (let i = 0; i < circles.length; ++i) {
+          circles[i].addEventListener('click',this.mouseclick)
+        }
     }
-
+    histoTitleClick =()=>{
+      console.log("title clicked")
+      console.log()
+      this.setState({isModalVisible:true})
+    }
+    handleModalOpen =(e)=>{
+      console.log("modal open")
+      let text =e.contentEl.getElementsByTagName('text')
+      let circles = e.contentEl.getElementsByTagName('circle')
+      text[0].style.setProperty('transform',"translateY(13px)")
+      for (let i = 0; i < circles.length; ++i) {
+        circles[i].style.setProperty('transform',"scale(1.3) translateY(-2px)")
+      }
+      console.log(text)
+    }
     render() {
-        const {selection}= this.state;
+        const {selection,isModalVisible}= this.state;
+        const customStyles = {
+          content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+          },
+        };
         return (
-            <div>
+            <div >
+            <Modal 
+              style={customStyles}
+              onRequestClose={()=>this.setState({isModalVisible:false})}
+              isOpen={this.state.isModalVisible}
+              onAfterOpen={this.handleModalOpen}
+              onAfterClose={(e)=>{
+                console.log("modal close")
+                console.log(e)}}
+              shouldCloseOnOverlayClick
+              >
+            <Histoslider 
+            formatLabelFunction={value=>{
+              return this.state.perc2value[Math.floor(value)]}}
+            barPadding={0}
+            barBorderRadius={0}
+            width={500} height={300}
+            selectedColor={this.props.color}
+    // An array of objects to create the histogram
+            data={this.state.data}
+    // How much to pad the slider and histogram by, defaults to 20
+            padding={20} 
+            handleLabelFormat={ "0.3P"}
+            selection={selection}
+            onChange={array => {
+              this.setState({
+                selection:array
+            })}}
+    // The extent of the selection, this doesn't have to be sorted (and you shouldn't sort it to store it)
+    // A function to handle a change in the selection
+    
+  />
+            </Modal>
             <Histoslider  
             formatLabelFunction={value=>{
               return this.state.perc2value[Math.floor(value)]}}
@@ -66,11 +139,7 @@ class HistoSelector extends Component {
     padding={20} 
     handleLabelFormat={ "0.3P"}
     selection={selection}
-    onClick={e=>{e.preventDefaut();console.log(e)}}
     onChange={array => {
-      this.state.isSelected=false;
-      // console.log(selection)
-    
       this.setState({
         selection:array
     })}}
@@ -78,7 +147,9 @@ class HistoSelector extends Component {
     // A function to handle a change in the selection
     
   />
-  <h4>{this.props.name}</h4>
+  <h4 style={{marginTop: '9px'}}><a onClick={this.histoTitleClick} style={{color:'black'}} className="unselectable hvr-grow">
+    {this.props.name}
+    </a></h4>
   </div>
         )
     }
